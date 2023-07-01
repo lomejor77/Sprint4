b66c2a474130cda60a4aff557b599ea40f174779
Alex Muñoz <lomejor77@gmail.com>
2023-07-01 13:27:51 -0400

    Sprint Modulo 4 correccion de errores de diseño

--- app/src/main/java/cl/awakelabs/sprint4/MainActivity.java
+++ app/src/main/java/cl/awakelabs/sprint4/MainActivity.java
@@ -2,13 +2,28 @@ package cl.awakelabs.sprint4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import cl.awakelabs.sprint4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnActiv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vSecond = new Intent(getBaseContext(), ActivitySecond.class);
+                startActivity(vSecond);
+            }
+        });
     }
 }
